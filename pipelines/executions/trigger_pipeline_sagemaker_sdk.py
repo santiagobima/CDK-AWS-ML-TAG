9fefe80b1025bafc7bb93888e41411b8e8f3f874
santiagobima <bimasantiago@gmail.com>
2024-10-17 17:13:13 +0200

    refactor

--- pipelines/executions/trigger_pipeline_sagemaker_sdk.py
+++ pipelines/executions/trigger_pipeline_sagemaker_sdk.py
@@ -1,39 +1,45 @@
 import os
-import sagemaker
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.workflow.pipeline_context import LocalPipelineSession
-from botocore.exceptions import NoCredentialsError, ClientError
+import sys
+
+# Añadir el directorio raíz del proyecto al PYTHONPATH
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_dir)
+print(f"Directorio raíz añadido al PYTHONPATH: {root_dir}")  # Verificar si es correcto
+
+from pipelines.definitions.lead_conversion_pipeline import LeadConversionFactory  
+
 
 # Verificar si estamos en modo local
 local_mode = os.getenv('LOCAL_MODE', 'false').lower() == 'true'
 
-try:
-    # Crear la sesión de SageMaker
-    if local_mode:
-        print("Ejecutando el pipeline en modo local")
-        session = LocalPipelineSession()
-    else:
-        print("Ejecutando el pipeline en la nube")
-        session = sagemaker.Session()
+# Crear la sesión de SageMaker
+if local_mode:
+    print("Ejecutando el pipeline en modo local")
+    session = LocalPipelineSession()
+else:
+    print("Ejecutando el pipeline en la nube")
+    session = sagemaker.Session()
 
-    # Definir el nombre del pipeline
-    pipeline_name = "example-pipeline"
+# Definir el nombre del pipeline
+pipeline_name = "example-pipeline"
 
-    # Cargar el pipeline
+if local_mode:
+    # Aquí, en lugar de ejecutar pipeline.start(), ejecutamos manualmente los pasos del pipeline.
+    print(f"Ejecutando pasos de pipeline {pipeline_name} localmente...")
+
+    # Aquí es donde deberías definir los pasos del pipeline
+    # Por ejemplo, podrías invocar directamente los scripts que definen tus pasos
+    os.system("python3 pipelines/sources/lead_conversion/evaluate.py")
+    os.system("python3 pipelines/sources/lead_conversion/simple_step.py")
+
+    print("Ejecución local del pipeline completada.")
+else:
+    # Para la nube
     pipeline = Pipeline(name=pipeline_name, sagemaker_session=session)
-
-    # Iniciar la ejecución del pipeline
-    print(f"Iniciando la ejecución del pipeline: {pipeline_name}")
     execution = pipeline.start()
 
     # Esperar a que el pipeline termine
     execution.wait()
-    print("Ejecución del pipeline completada con éxito")
-
-except NoCredentialsError as e:
-    print("Error: No se encontraron credenciales de AWS. Verifica tu configuración.")
-except ClientError as e:
-    print(f"Error al ejecutar el pipeline: {e}")
-except Exception as e:
-    print(f"Error inesperado: {e}")
-
+    print("Pipeline execution completed.")
