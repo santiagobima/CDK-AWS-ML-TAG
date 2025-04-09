5bfb82b65cd789a1b4383f4c8cb411ee69135830
santiagobima <bimasantiago@gmail.com>
2025-04-09 19:56:04 +0200

    updates.

--- pipelines/lead_conversion_rate/steps/simple_step.py
+++ pipelines/lead_conversion_rate/steps/simple_step.py
@@ -1,10 +1,38 @@
 # simple_step.py
 
 import argparse
+import logging
+import sys
+import os
 
-# simple_step.py
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+if not logger.handlers:
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 if __name__ == "__main__":
-    print("Executing local processing step...")
-    print("This step doesn't require S3 inputs or outputs.")
-    print("Processing step 2 completed successfully!")
+    logger.info(" This step doesn't require S3 inputs or outputs.")
+    logger.info(" Processing SIMPLE_STEP completed successfully!")
+    
+    
+ try:
+        input_contents = os.listdir('/opt/ml/processing/input')
+        logger.info(f"📁 Input directory contents: {input_contents}")
+    except FileNotFoundError:
+        input_contents = []
+        logger.warning("⚠️ Input directory not found.")
+
+    # Guardar log en un archivo de salida
+    output_path = "/opt/ml/processing/output/log_check.txt"
+    with open(output_path, "w") as f:
+        f.write("✅ simple_step.py completed successfully.\n")
+        f.write(f"📁 Input contents: {input_contents}\n")
+
+    logger.info(f"📝 Output written to {output_path}")
+    
+    
